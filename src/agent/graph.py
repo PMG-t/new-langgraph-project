22896b20c43a216ab04f701721903d274dbed8db
PimpMyGit <tommasoredaelli276@gmail.com>
2025-03-18 15:00:59 +0100

    refactor

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -1,35 +1,16 @@
-"""Define a simple chatbot agent.
-
-This agent returns a predefined response without using an actual LLM.
+"""
+Defining agent graph
 """
 
-from typing import Any, Dict
-
-from langchain_core.runnables import RunnableConfig
 from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+
+from agent.names import *
 
 from agent.configuration import Configuration
 from agent.states.state import State
 
-from langchain_core.tools import tool
-from langchain_openai import ChatOpenAI
-
-from typing_extensions import TypedDict, Literal
-from typing import Annotated
-from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph, START, END, MessagesState
-from langgraph.checkpoint.memory import MemorySaver
-from langgraph.types import Command, interrupt
-from langchain_openai import ChatOpenAI
-from langchain_core.tools import tool
-from langgraph.prebuilt import ToolNode
-from langchain_core.messages import AIMessage
-# from IPython.display import Image, display
-from langchain_core.runnables.graph import CurveStyle, MermaidDrawMethod, NodeStyles
-from langchain_core.messages import RemoveMessage
-
-
-from agent.names import *
 from agent.nodes import chatbot
 from agent.nodes import chatbot_router
 from agent.nodes import demo_get_precipitation_data_tool_validator
